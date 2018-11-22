614634267e191afa9d3dbc5b5f87fadae2da49bc
Men3m <heroshima90@yahoo.com>
2018-11-22 22:43:45 +0200

    I did Splash screen and that was a test.

---
+++ app/src/main/java/com/project/csed/smartlearning/Splash.java
@@ -0,0 +1,26 @@
+package com.project.csed.smartlearning;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(Splash.this,MainActivity.class);
+                Splash.this.startActivity(mainIntent);
+                Splash.this.finish();
+            }
+        }, 1000);
+    }
+
+}
