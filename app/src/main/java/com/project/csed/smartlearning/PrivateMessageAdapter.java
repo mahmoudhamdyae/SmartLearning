021f8f319589e1f79e4162c342ca29fecfb5b6cb
Men3m <heroshima90@yahoo.com>
2019-04-16 19:50:39 +0200

    Changing PrivateChat shape

--- app/src/main/java/com/project/csed/smartlearning/PrivateMessageAdapter.java
+++ app/src/main/java/com/project/csed/smartlearning/PrivateMessageAdapter.java
@@ -1,16 +1,27 @@
 package com.project.csed.smartlearning;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
+import static android.content.Context.MODE_PRIVATE;
+
 public class PrivateMessageAdapter extends ArrayAdapter<PrivateMessagePOJO> {
+    String username;
+    SharedPreferences sharedPreferences ;
+
+
+
 
 
 
@@ -20,7 +31,12 @@ public class PrivateMessageAdapter extends ArrayAdapter<PrivateMessagePOJO> {
         // Because this is a custom adapter for two TextViews and an ImageView, the adapter is not
         // going to use this second argument, so it can be any value. Here, we used 0.
         super(context, 0, messageList);
+        sharedPreferences=context.getSharedPreferences("userinfo",context.MODE_PRIVATE);
+        username= (String) sharedPreferences.getString("username","empty");
+        Toast.makeText(context, "hello "+username, Toast.LENGTH_SHORT).show();
     }
+
+
 
     /**
      * Provides a view for an AdapterView (ListView, GridView, etc.)
@@ -41,7 +57,11 @@ public class PrivateMessageAdapter extends ArrayAdapter<PrivateMessagePOJO> {
 
 
 
+
         }
+
+        RelativeLayout relativeLayout1 = listItemView.findViewById(R.id.relativeLayout1);
+        RelativeLayout relativeLayout2 = listItemView.findViewById(R.id.relativeLayout2);
 
         // Get the {@link AndroidFlavor} object located at this position in the list
         PrivateMessagePOJO currentMessage = getItem(position);
@@ -63,6 +83,39 @@ public class PrivateMessageAdapter extends ArrayAdapter<PrivateMessagePOJO> {
         // Get the version number from the current AndroidFlavor object and
         // set this text on the number TextView
         dateTextView.setText(currentMessage.getDate());
+
+
+//        PrivateMessagePOJO currentMessage = getItem(position);
+
+
+        //From here i add this to View the messages face each other
+
+//        TextView senderTextView2 =  listItemView.findViewById(R.id.private_chat_sender2);
+//        senderTextView2.setText(currentMessage.getSenderName());
+//
+//
+//        TextView messageTextView2 =  listItemView.findViewById(R.id.private_chat_message2);
+//        messageTextView2.setText(currentMessage.getMessage());
+//
+//
+//        TextView dateTextView2 =  listItemView.findViewById(R.id.private_chat_time2);
+//        dateTextView2.setText(currentMessage.getDate());
+//        String usersender=currentMessage.getSenderName();
+//        Toast.makeText(getContext(), ""+usersender, Toast.LENGTH_SHORT).show();
+//        if (username ==usersender)
+//        {
+//            relativeLayout1.setVisibility(View.VISIBLE);
+//            relativeLayout2.setVisibility(View.GONE);
+////            Toast.makeText(getContext(), "true", Toast.LENGTH_SHORT).show();
+//
+//        }else
+//        {
+//            relativeLayout1.setVisibility(View.GONE);
+//            relativeLayout2.setVisibility(View.VISIBLE);
+////            Toast.makeText(getContext(), "fals", Toast.LENGTH_SHORT).show();
+//
+//        }
+
 
 //        // Find the ImageView in the list_item.xml layout with the ID list_item_icon
 
